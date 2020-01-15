f52a94b758f17214637576e9444b14b09348827d
Alex <agallagher55@gmail.com>
2020-01-14 21:54:04 -0400

    working

--- 1/scripts.js
+++ 1/scripts.js
@@ -1,21 +1,56 @@
-const results = document.querySelector('.results p')
-results.textContent = "IT WORKS"
+// const results = document.querySelector('.results p')
+const buttons = document.querySelectorAll('button')
 
-console.log(results)
+// results.textContent = "IT WORKS"
 
 const targetKeys = ['KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK']
+let sounds = ['boom.wav', 'clap.wav', 'hihat.wav', 'kick.wav', 'openhat.wav',
+                'ride.wav', 'snare.wav', 'tink.wav', 'tom.wav']
+sounds = sounds.map(x => 'sounds/' + x);
 
-// Populate results box with keyboard
-document.addEventListener('keydown', function(e) {
+let keyMap = {}
+
+targetKeys.forEach(function(key, index) {
+    keyMap[key] = sounds[index]
+});
+console.log(keyMap)
+
+const playMusic = function(e){
     e.preventDefault()
+    key = e.code;
 
-    if (targetKeys.includes(e.code)) {
-        results.textContent = e.code;
+    if (targetKeys.includes(key)) {
+        // results.textContent = key;
+        const audio = new Audio(keyMap[key])
+        audio.play()
+
     }
     else {
-        console.log(`You pressed: ${e.code}`)
-        results.textContent = '';
+        console.log(`You pressed: ${key}`)
+        // results.textContent = '...';
     }
+}
 
-})
+const highlightButton = async function(e) {
+    console.log("highlight button function")
+    // Which button was pressed? 
+    // Get key Letter
+    // The index of that letter in key list will tell you what button to highlight
+    const key = e.code;
+    if (targetKeys.includes(key)) {
+        const keyIndex = targetKeys.indexOf(key)
+        buttons[keyIndex].classList.add('button-highlight')
+    }
+}
 
+const unhighlightButton = function(e) {
+    const key = e.code;
+    if (targetKeys.includes(key)) {
+        const keyIndex = targetKeys.indexOf(key)
+        buttons[keyIndex].classList.remove('button-highlight')
+    }
+}
+
+document.addEventListener('keydown', playMusic)
+document.addEventListener('keydown', highlightButton)
+document.addEventListener('keyup', unhighlightButton)
